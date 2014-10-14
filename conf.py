bbed109c31097bd64f52499618bcd5ed5b0319ef
Luper Rouch <luper.rouch@gmail.com>
2014-10-14 10:20:17 +0200

    updated version

--- conf.py
+++ conf.py
@@ -51,7 +51,7 @@ copyright = u'2013, Luper Rouch'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '2.0'
 # The full version, including alpha/beta/rc tags.
 release = '0.1'
 
